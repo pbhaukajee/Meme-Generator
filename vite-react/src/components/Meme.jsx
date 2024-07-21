04c473de49e5edb26eaa408ae81ac827b32f59b7
Priyanka <pbhaukajee@gmail.com>
2024-07-21 11:13:52 -0500

    update useStates to get random Images

--- vite-react/src/components/Meme.jsx
+++ vite-react/src/components/Meme.jsx
@@ -2,11 +2,21 @@ import React from "react";
 import memesData from "../memesData";
 
 export default function Meme() {
-  const [memeImage, setMemeImage] = React.useState("");
+  const [meme, setMeme] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+  });
+  const [allMemeImages, setAllMemeImages] = React.useState(memesData);
+
   function getMemeImage() {
-    const memesArray = memesData.data.memes;
+    const memesArray = allMemeImages.data.memes;
     let randomNumber = Math.floor(Math.random() * memesArray.length);
-    setMemeImage(memesArray[randomNumber].url);
+    const url = memesArray[randomNumber].url;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      randomImage: url,
+    }));
   }
 
   return (
@@ -35,7 +45,7 @@ export default function Meme() {
           Get a new meme image 🖼
         </button>
       </div>
-      <img className="meme--image" src={memeImage} alt="image" />
+      <img className="meme--image" src={meme.randomImage} alt="image" />
     </main>
   );
 }
